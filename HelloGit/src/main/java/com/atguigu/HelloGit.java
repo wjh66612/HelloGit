9d9ea5492105b62659c4e0ce2506a358bc5365e1
wjh66612 <1529659488@qq.com>
2020-09-06 10:43:18 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author PiaoBo
+ * @create 2020-09-06 10:21
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit.main");
+    }
+}
